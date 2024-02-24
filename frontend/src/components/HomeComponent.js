528b698c7aee7c3d9ead65398ba78b5ca19f221c
serralva-ruben <91135825+serralva-ruben@users.noreply.github.com>
2024-02-24 14:53:08 +0100

    video added to background

--- frontend/src/components/HomeComponent.js
+++ frontend/src/components/HomeComponent.js
@@ -2,10 +2,14 @@ import React from 'react';
 import { NavbarComponent } from './NavbarComponent.js';
 import HomeText from './text.js';
 import image1 from '../assets/img/worldwide-shipping.png';
+import video1 from '../assets/video/video.mp4';
 
 const HomeComponent = () => {
     return (
         <div style={homeStyle}>
+            <video autoPlay loop muted style={backgroundVideoStyle}>
+                <source src={video1} />
+            </video>
             <NavbarComponent />
             <div style={homePageStyle}>
                 <div>
@@ -34,15 +38,15 @@ const HomeComponent = () => {
                         </div>
                     </div>
                 </div>
-                <div style={imgDiv}><img src={image1} style={imgStyle}/></div>
-
+                {false ? <div style={imgDiv}><img src={image1} style={imgStyle}/></div> : null}
             </div>
         </div>
     );
 };
 
 const homeStyle = {
-    backgroundColor: '#2164C9',
+    //backgroundColor: '#2164C9',
+    
 };
 
 const imgDiv = {
@@ -70,8 +74,8 @@ const homePageStyle = {
 const bigText = {
     //border: '1px solid red',
     height: 'auto',
-    width: '35vw',
-    marginTop : '35vh',
+    width: '40vw',
+    marginTop : '25vh',
     marginLeft: '10vw',
     marginBottom: '0.2em',
     textAlign: 'left',
@@ -83,7 +87,7 @@ const bigText = {
 const smallText = {
     //border: '1px solid red',
     height: 'auto',
-    width: '30vw',
+    width: '40vw',
     marginTop : '0vh',
     marginLeft: '10vw',
     textAlign: 'left',
@@ -94,19 +98,20 @@ const smallText = {
 const buttonDiv ={
     //border: '1px solid red',
     height: '2.5em',
-    width: '20vw',
+    width: 'calc(10em + 10vw)',
     marginLeft: '10vw',
     textAlign: 'left',
     fontSize: '1em',
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-between',
+    marginTop : '2em',
 }
 
 const buttonStyle = {
     border: '3px solid #17468D',
     borderRadius: '15px',
-    width: '8vw',
+    width: '10em',
     textAlign: 'left',
     fontSize: '0.8em',
     textAlign: 'center',
@@ -119,18 +124,19 @@ const statsDiv = {
     display: 'flex',
     flexDirection: 'row',
     justifyContent: 'space-around',
-    width: '20vw',
-    marginLeft: '10vw',
-    marginTop: '3em',
+    width: '10em',
+    marginLeft: '15vw',
+    marginTop: '2em',
     color: 'white',
 }
 
 const statValueStyle = {
-    fontSize: '3em',
-    color: '#17468D',
+    fontSize: '3em', // Update the font size here
+    color: 'white',
     textAlign: 'center',
     fontWeight: 'bold',
     fontStyle: 'italic',
+    textShadow: '0px 0px 0px black', // Add a black border around the font
 }
 
 const statLabelStyle = {
@@ -140,12 +146,25 @@ const statLabelStyle = {
     fontWeight: 'bold',
     fontStyle: 'italic',
     marginTop: '0.5em',
+    textShadow: '1px 2px 0px black', // Add a black border around the font
+
 }
 
 const dividerStyle = {
     border: '1px solid lightgrey',
     width: '0.01em',
     marginTop: '1em',
+    marginInline: '1em',
+}
+
+const backgroundVideoStyle = {
+    position: 'fixed',
+    top: 0,
+    left: 0,
+    width: '100%',
+    height: '100%',
+    zIndex: -1,
+    objectFit: 'cover',
 }
 
 export default HomeComponent;
--- frontend/src/components/NavbarComponent.js
+++ frontend/src/components/NavbarComponent.js
@@ -6,13 +6,15 @@ export const NavbarComponent = () => {
       <div style={leftStyle}>
           <div style= {{margin: 'auto'}}>DockChain</div>
       </div>
+      <div style= {{width:'20vw'}}/>
       <ul style={centerButtonsStyle}>
-        <li>Home</li>
-        <li>Discover</li>
-        <li>Marketplace</li>
-        <li>Support</li>
-        <li>Community</li>
+        <button className="click" style={navBarButtonsStyle}>Home</button>
+        <button className="click" style={navBarButtonsStyle}>Discover</button>
+        <button className="click" style={navBarButtonsStyle}>Marketplace</button>
+        <button className="click" style={navBarButtonsStyle}>Support</button>
+        <button className="click" style={navBarButtonsStyle}>Community</button>
       </ul>
+      <div style= {{width:'20vw'}}/>
       <div style={rightStyle}>
         <button style={buttonStyle} className="click">Connect wallet</button>
       </div>
@@ -24,27 +26,34 @@ const centerButtonsStyle = {
   listStyleType: 'none',
   display: 'flex',
   alignItems: 'center',
-  justifyContent: 'space-between',
+  justifyContent: 'center', // Updated property
   //border: '3px solid red',
-  height: '10vh',
-  width: '50vw',
+  height: '4em',
+  flex: 1, // Added property
   padding: 0,
 };
+
+const navBarButtonsStyle = {
+  border: 'none',
+  background: 'none',
+  height: '4em',
+}
 
 const leftStyle = {
   //border: '3px solid blue',
   height: '4em',
-  width: '25vw',
+  width: '20vw',
   textAlign: 'center',
   display: 'grid',
-  fontSize: '1.5em',
+  fontSize: 'calc(0.6vw + 0.8em)',
+  paddingLeft: '1vw',
   fontStyle: 'Bold',
 }
 
   const rightStyle = {
   //border: '3px solid yellow',
   height: '4em',
-  width: '25vw',
+  width: '20vw',
   display: 'grid',
   margin: 'auto',
 }
@@ -65,7 +74,7 @@ const buttonStyle = {
   padding: '0.5em',
   border: '1px solid #17468D',
   borderRadius: '15px',
-  width: '8vw',
+  width: '8em',
   textAlign: 'center',
   fontSize: '0.8em',
   margin: 'auto',
